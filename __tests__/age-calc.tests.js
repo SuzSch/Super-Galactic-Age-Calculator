0edad83502eb65c9e369e2d9698a9f0d58b05c7e
Suzanne Schuber <suzanneschuber@gmail.com>
2023-06-23 15:31:32 -0700

    Good fail for past birthday test, WIP to refactor business logic.

--- __tests__/age-calc.tests.js
+++ __tests__/age-calc.tests.js
@@ -7,24 +7,27 @@ describe('Planet Age Calculator', () => {
     userAge = new AgeCalc(10);
   });
 
-  test('Should get the users age in Earth years', () => {
-    expect(userAge.age).toEqual(10)
-  })
+  // test('Should get the users age in Earth years', () => {
+  //   expect(userAge.age).toEqual(10)
+  // })
 
-  test('Should get the users age in Mercury years', () => {
-    expect(userAge.mercuryAge()).toEqual(2.4)
-  })
+  // test('Should get the users age in Mercury years', () => {
+  //   expect(userAge.mercuryAge()).toEqual(2.4)
+  // })
 
-  test('Should get the users age in Venus years', () => {
-    expect(userAge.venusAge()).toEqual(6.2)
-  })
+  // test('Should get the users age in Venus years', () => {
+  //   expect(userAge.venusAge()).toEqual(6.2)
+  // })
 
-  test('Should get the users age in Mars years', () => {
-    expect(userAge.marsAge()).toBeCloseTo(18.8);
-  })
+  // test('Should get the users age in Mars years', () => {
+  //   expect(userAge.marsAge()).toBeCloseTo(18.8);
+  // })
 
-  test('Should get the user age in Jupiter years', () => {
-    expect(userAge.jupiterAge()).toEqual(118.6);
+  // test('Should get the user age in Jupiter years', () => {
+  //   expect(userAge.jupiterAge()).toEqual(118.6);
+  // })
+  test('Should determine how many years have passed on each planet since a past birtday', () => {
+    const pastAge = new AgeCalc(7);
+    expect((userAge) - (pastAge)).toEqual(3)
   })
-  test('Should determine how many years have passed on each planet since a past birtday')
 });
--- src/age-calc.js
+++ src/age-calc.js
@@ -2,19 +2,22 @@ export class AgeCalc {
   constructor(earthAge) {
     this.age = earthAge;
     this.ageConversion = {
-
-    }
+      mercury: 0.24,
+      venus: 0.62,
+      mars: 1.88,
+      jupiter: 11.86,
+    };
   }
-  mercuryAge() {
-    return this.age * .24;
-  }
-  venusAge() {
-    return this.age * .62;
-  }
-  marsAge() {
-    return this.age * 1.88;
-  }
-  jupiterAge() {
-    return this.age * 11.86;
-  }
-};
+}
+  // mercuryAge() {
+  //   return this.age * .24;
+  // }
+  // venusAge() {
+  //   return this.age * .62;
+  // }
+  // marsAge() {
+  //   return this.age * 1.88;
+  // }
+  // jupiterAge() {
+  //   return this.age * 11.86;
+  // }
