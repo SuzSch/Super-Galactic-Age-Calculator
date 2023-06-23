bf8af00aaef0c0f7f9445198c1c299fb4d6eb664
Suzanne Schuber <suzanneschuber@gmail.com>
2023-06-23 11:47:24 -0700

    good fail for test 2 Mercury.

--- __tests__/age-calc.tests.js
+++ __tests__/age-calc.tests.js
@@ -3,7 +3,12 @@ import { AgeCalc } from './../src/age-calc.js';
 describe('Planet Age Calculator', () => {
 
   test('get the users age in earth years', () => {
-    const earthAge = new AgeCalc(10);
-    expect(earthAge.age).toEqual(10)
+    const earth = new AgeCalc(10);
+    expect(earth.age).toEqual(10)
+  })
+
+  test('get the users age in Mercury years', () => {
+    const mercury = new AgeCalc(10);
+    expect(mercury.age).toEqual(2.4)
   })
 });
