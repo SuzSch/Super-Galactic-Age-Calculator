480c8457995068f14c07895549eb94d49a786763
Suzanne Schuber <suzanneschuber@gmail.com>
2023-06-23 14:01:40 -0700

    add forEach() to dry up test code.

--- __tests__/age-calc.tests.js
+++ __tests__/age-calc.tests.js
@@ -1,29 +1,29 @@
 import { AgeCalc } from './../src/age-calc.js';
 
 describe('Planet Age Calculator', () => {
+  let userAge;
+
+  beforeEach(() => {
+    userAge = new AgeCalc(10);
+  });
 
   test('Should get the users age in Earth years', () => {
-    const userAge = new AgeCalc(10);
     expect(userAge.age).toEqual(10)
   })
 
   test('Should get the users age in Mercury years', () => {
-    const userAge = new AgeCalc(10);
     expect(userAge.mercuryAge()).toEqual(2.4)
   })
 
   test('Should get the users age in Venus years', () => {
-    const userAge = new AgeCalc(10);
     expect(userAge.venusAge()).toEqual(6.2)
   })
 
   test('Should get the users age in Mars years', () => {
-    const userAge = new AgeCalc(10);
     expect(userAge.marsAge()).toBeCloseTo(18.8);
   })
 
   test('Should get the user age in Jupiter years', () => {
-    const userAge = new AgeCalc(10);
     expect(userAge.jupiterAge()).toEqual(118.6);
   })
 });
