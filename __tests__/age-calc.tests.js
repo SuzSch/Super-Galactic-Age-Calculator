878bdf2481a2ae9930a1bd5ec8cc3a00bf3c6f8e
Suzanne Schuber <suzanneschuber@gmail.com>
2023-06-23 16:32:00 -0700

    update tests to refect updated business logic.

--- __tests__/age-calc.tests.js
+++ __tests__/age-calc.tests.js
@@ -15,17 +15,17 @@ describe('Planet Age Calculator', () => {
     expect(userAge.ageOnPlanet('mercury')).toEqual(2.4)
   })
 
-  // test('Should get the users age in Venus years', () => {
-  //   expect(userAge.venusAge()).toEqual(6.2)
-  // })
+  test('Should get the users age in Venus years', () => {
+    expect(userAge.ageOnPlanet('venus')).toEqual(6.2)
+  })
 
-  // test('Should get the users age in Mars years', () => {
-  //   expect(userAge.marsAge()).toBeCloseTo(18.8);
-  // })
+  test('Should get the users age in Mars years', () => {
+    expect(userAge.ageOnPlanet('mars')).toBeCloseTo(18.8);
+  })
 
-  // test('Should get the user age in Jupiter years', () => {
-  //   expect(userAge.jupiterAge()).toEqual(118.6);
-  // })
+  test('Should get the user age in Jupiter years', () => {
+    expect(userAge.ageOnPlanet('jupiter')).toEqual(118.6);
+  })
   // test('Should determine how many years have passed on each planet since a past birtday', () => {
   //   const pastAge = new AgeCalc(7);
   //   expect((userAge) - (pastAge)).toEqual(3)
